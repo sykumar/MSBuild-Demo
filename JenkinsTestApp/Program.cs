ae0b89a646dd1ad2f829a63038c21658f0bb5adf
Yogesh Kumar <sykumar@tricoresolutions.com>
2017-08-08 13:57:57 +0530

    Initials

---
+++ JenkinsTestApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JenkinsTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10; i++)
+                Console.WriteLine("Hello World ..." + i);
+        }
+    }
+}
